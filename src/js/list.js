339bcde086d2b34ade67b7e1dd376f7454d6c7ae
이인구 <iingu@iinguui-MacBookAir.local>
2021-08-23 20:03:49 +0900

    LIST PAGE DONE

--- src/js/list.js
+++ src/js/list.js
@@ -1,13 +1,18 @@
-/*
+const arrows = document.querySelectorAll(".arrow");
+const movieList = document.querySelectorAll(".movie_list");
 
-window.onload = function() {
-    load_image();
-}
-function load_image() {
-    const content = document.querySelector(".content");
-    content.style.backgroundImage = `url("src/img/poster1.jpeg")`;
-    content.style.backgroundSize = "100% 100%";
-    content.style.repeat = "no-repeat";
-}
-
-*/
+arrows.forEach((arrow, i)=>{
+    const itemNumber = movieList[i].querySelectorAll("img").length;
+    let clickCounter = 0;
+    arrow.addEventListener("click", () => {
+        clickCounter++;
+        if(itemNumber - (5 + clickCounter) >= 0){
+            movieList[i].style.transform = `translateX(${
+                movieList[i].computedStyleMap().get("transform")[0].x.value
+            -345}px)`;
+        } else{
+            movieList[i].style.transform = "translateX(0)"
+            clickCounter = 0;
+        }
+    });
+});
